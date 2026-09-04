c0b6dad7efc22ecb085afb326a055b1de61d94ca
Javier Alejandro  Arrieta Roja <jarrieta@gomezpinedaabogados.com>
2026-09-04 13:10:19 -0500

    cambio global

--- Frontend/api/client.ts
+++ Frontend/api/client.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: 'process.env.NEXT_PUBLIC_API_URL', // Replace with your API base URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // Replace with your API base URL
 });
